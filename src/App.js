8a432196f491861ab54ebd7666f3e07fdedb1d6e
vmurali100 <vmurali100@gmail.com>
2021-09-27 08:01:42 +0530

    trying conflicts

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { Nav } from "./Nav";
 import { Progress } from "./Progress";
 import "./style.css";
 
+import ReactPlayer from 'react-player'
 export default function App() {
   return (
     <div>
       <Nav />
-      <Progress />
+      {/* <Progress /> */}
+      <div className="videoWrapper">
+      <ReactPlayer controls url="./003 Tutorial 3- Python Class And Objects.mp4" />
+
+        </div>      
     </div>
   );
 }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -9,7 +9,7 @@ export const Nav = () => {
                     <div class="collapse navbar-collapse" id="navbarCollapse">
                         <ul class="navbar-nav me-auto mb-2 mb-md-0">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#" style={{ color: "FFFFFF" }}>Home of Australia’s Most Loved and Trusted Insulation Brands</a>
+                                <a class="nav-link active" aria-current="page" href="#" style={{ color: "FFFFFF" }}>Progress Bar</a>
                             </li>
                         </ul>
                         <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
@@ -17,6 +17,7 @@ export const Nav = () => {
                             <a class="me-3 py-2  text-decoration-none" href="#">Enterprise</a>
                             <a class="me-3 py-2  text-decoration-none" href="#">Support</a>
                         </nav>
+                        <h2>This changes i am doing in Master Branch</h2>
                     </div>
                 </div>
             </nav>
